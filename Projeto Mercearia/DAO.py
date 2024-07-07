928c59ba1855bcc75c9ad9ab2aafb31dc27c4a4f
Marco Nogueira <marco.nogueira@sistemafiep.org.br>
2024-07-07 17:33:38 -0300

    Completado DaoCategoria e DaoVenda

--- Projeto Mercearia/DAO.py
+++ Projeto Mercearia/DAO.py
@@ -3,29 +3,29 @@ from Models import *
 class DaoCategoria:
     @classmethod
     def salvar(cls, categoria):
-        with open('categoria.txt', 'a') as arq:
+        with open('Projeto Mercearia/categoria.txt', 'a') as arq:
             arq.writelines(categoria)
             arq.writelines('\n')
 
     @classmethod
     def ler(cls):
-        with open('categoria.txt', 'r') as arq:
+        with open('Projeto Mercearia/categoria.txt', 'r') as arq:
             cls.categoria = arq.readlines()
 
         cls.categoria = list(map(lambda x: x.replace('\n', ''), cls.categoria))
+        print(cls.categoria)    
         
-        # cat = []
-        # for i in cls.categoria:
-        #     cat.append(Categoria(i))
-        # return cat
-        print(cls.categoria)
-
+        cat = []
+        for i in cls.categoria:
+            cat.append(Categoria(i))
+        return cat
+        
+    
 class DaoVenda:
     @classmethod
     def salvar(cls, venda: Venda):
-        with open('venda.txt', 'a') as arq:
-            arq.writelines(venda.id + "|"
-                            + venda.itensVendidos.nome + "|"
+        with open('Projeto Mercearia/venda.txt', 'a') as arq:
+            arq.writelines(venda.itensVendidos.nome + "|"
                             + venda.itensVendidos.preco + "|"
                             + venda.itensVendidos.categoria + "|"
                             + venda.vendedor + "|"
@@ -36,24 +36,26 @@ class DaoVenda:
 
     @classmethod
     def ler(cls):
-        with open('venda.txt', 'r') as arq:
+        with open('Projeto Mercearia/venda.txt', 'r') as arq:
             cls.venda = arq.readlines()
             
         cls.venda = list(map(lambda x: x.replace('\n', ''), cls.venda))
         cls.venda = list(map(lambda x: x.split('|'), cls.venda))
-        
+        print(cls.venda) 
+
         vend = []
         for i in cls.venda:
-            vend.append(Venda(Produtos(i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7])))
-        return Venda
-        
+            vend.append(Venda(Produtos(i[0], i[1], i[2]), i[3], i[4], i[5], i[6]))
+        return vend
 
-x = Produtos('1', 'Banana', '12', 'Frutas')     
-y = Venda('1', x, 'Marco', 'Pedro', '3')
+DaoCategoria.salvar('Frutas')
+DaoCategoria.salvar('Verduras')
+DaoCategoria.salvar('Legumes')
+DaoCategoria.ler()
+
+x = Produtos('Banana', '12', 'Frutas')     
+y = Venda(x, 'Marco', 'Pedro', '3')
 DaoVenda.salvar(y)
-# DaoVenda.ler()
+a = DaoVenda.ler()
+print(a[0].itensVendidos.nome)
 
-# DaoCategoria.salvar('Frutas')
-# DaoCategoria.salvar('Verduras')
-# DaoCategoria.salvar('Legumes')
-# DaoCategoria.ler()
--- Projeto Mercearia/Models.py
+++ Projeto Mercearia/Models.py
@@ -1,35 +1,30 @@
 from datetime import datetime
 
 class Categoria:
-    def __init__(self, id, nome):
-        self.id = id
+    def __init__(self, nome):
         self.nome = nome
         
 class Produtos:
-    def __init__(self, id, nome, preco, categoria: Categoria):
-        self.id = id
+    def __init__(self, nome, preco, categoria: Categoria):
         self.nome = nome
         self.preco = preco
         self.categoria = categoria
         
 class Estoque:
-    def __init__(self, id, produto: Produtos, quantidade):
-        self.id = id
+    def __init__(self, produto: Produtos, quantidade):
         self.produto = produto
         self.quantidade = quantidade
         
 class Venda:
-    def __init__(self, id, itensVendidos: Produtos, vendedor, comprador, quantidadeVendida, data = datetime.now().strftime("%d/%m/%y")):
-        self.id = id
-        self.data = data
+    def __init__(self, itensVendidos: Produtos, vendedor, comprador, quantidadeVendida, data = datetime.now().strftime("%d/%m/%y")):
         self.itensVendidos = itensVendidos
         self.vendedor = vendedor
         self.comprador = comprador
         self.quantidadeVendida = quantidadeVendida
+        self.data = data
         
 class Fornecedor:
-    def __init__(self, id, nome, cnpj, telefone, endereco, categoria: Categoria):
-        self.id = id
+    def __init__(self, nome, cnpj, telefone, endereco, categoria: Categoria):
         self.nome = nome
         self.cnpj = cnpj
         self.telefone = telefone
@@ -37,8 +32,7 @@ class Fornecedor:
         self.categoria = categoria
     
 class Pessoa:
-    def __init__(self, id, nome, cpf, telefone, email, endereco):
-        self.id = id
+    def __init__(self, nome, cpf, telefone, email, endereco):
         self.nome = nome
         self.cpf = cpf
         self.telefone = telefone
@@ -46,11 +40,11 @@ class Pessoa:
         self.endereco = endereco
 
 class Funcionario(Pessoa):
-    def __init__(self, clt, id, nome, cpf, telefone, email, endereco):
+    def __init__(self, clt, nome, cpf, telefone, email, endereco):
         self.clt = clt
-        super(Funcionario, self).__init__(id, nome, cpf, telefone, email, endereco)
+        super(Funcionario, self).__init__(nome, cpf, telefone, email, endereco)
         
 class Cliente(Pessoa):
-    def __init__(self, id, nome, cpf, telefone, email, endereco, numeroFidelizacao):
+    def __init__(self, nome, cpf, telefone, email, endereco, numeroFidelizacao):
         self.numeroFidelizacao = numeroFidelizacao
-        super(Cliente, self).__init__(id, nome, cpf, telefone, email, endereco)
+        super(Cliente, self).__init__(nome, cpf, telefone, email, endereco)
