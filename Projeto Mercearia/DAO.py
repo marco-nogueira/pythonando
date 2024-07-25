12bfcc659cca0727365280152d829dfc66d512e4
Marco Aurelio Gibellato Nogueira <marconogueira.ti@gmail.com>
2024-07-24 21:28:23 -0300

    Adiciona DAOPessoa e DAOFuncionario

--- Projeto Mercearia/DAO.py
+++ Projeto Mercearia/DAO.py
@@ -19,6 +19,7 @@ class DaoCategoria:
         for i in cls.categoria:
             cat.append(Categoria(i))
         return cat
+    
 class DaoVenda:
     @classmethod
     def salvar(cls, venda: Venda):
@@ -69,6 +70,81 @@ class DaoEstoque:
             for i in cls.estoque:
                 est.append(Estoque(Produtos(i[0], i[1], i[2], i[3])))
         return est
+
+class DaoFornecedor:
+    @classmethod
+    def salvar(cls, fornecedor: Fornecedor):
+        with open('fornecedores.txt', 'a') as arq:
+            arq.writelines(fornecedor.nome + "|"
+                           + fornecedor.cnpj + "|""
+                           + fornecedor.telefone + "|"
+                           + fornecedor.categoria)
+            arq.writelines('\n')
+
+    @classmethod
+    def ler(cls):
+        with open('fornecedores.txt', 'r') as arq:
+            cls.fornecedores = arq.readlines()
+
+        cls.fornecedores = list(map(lambda x: x.replace('\n', ''), cls.fornecedores))
+        cls.fornecedores = list(map(lambda x: x.split('|'), cls.fornecedores))
+        forn = []
+        for i in cls.fornecedores:
+            forn.append(Fornecedor(i[0], i[1], i[2], i[3]))
+
+        return forn
+    
+class DaoPessoa:
+    @classmethod
+    def salvar(cls, pessoas: Pessoa):
+        with open('clientes.txt', 'a') as arq:
+            arq.writelines(pessoas.nome + "|"
+                           + pessoas.telefone + "|"
+                           + pessoas.cpf + "|"
+                           + pessoas.email + "|"
+                           + pessoas.endereco)
+            arq.writelines('\n')
+
+    @classmethod
+    def ler(cls):
+        with open('clientes.txt', 'r') as arq:
+            cls.clientes = arq.readlines
+
+        cls.clientes = list(map(lambda x: x.replace('\n', ''), cls.clientes))
+        cls.clientes = list(map(lambda x: x.split('|'), cls.clientes))
+        
+        clientes = []
+        for i in cls.clientes:
+            clientes.append(Pessoa(i[0], i[1], i[2], i[3], i[4]))
+
+        return clientes
+    
+class DaoFuncionario:
+    @classmethod
+    def salvar(cls, funcionario: Funcionario):
+        with open('funcionarios.txt', 'a') as arq:
+            arq.writelines(funcionario.clt + "|"
+                           + funcionario.nome + "|"
+                           + funcionario.telefone + "|"
+                           + funcionario.cpf + "|"
+                           + funcionario.email + "|"
+                           + funcionario.endereco)
+            arq.writelines('\n')
+
+    @classmethod
+    def ler(cls):
+        with open('funcionarios.txt', 'r') as arq:
+            cls.funcionarios = arq.readlines()
+
+        cls.funcionarios = list(map(lambda x: x.replace('\n', ''), cls.funcionarios))
+        cls.funcionarios = list(map(lambda x: x.split('|'), cls.funcionarios))
+        
+        funcionario = []
+        for i in cls.funcionarios:
+            funcionario.append(Funcionario(i[0], i[1], i[2], i[3], i[4], i[5]))
+
+        return funcionario
+        
 
 # Cria Categorias
 DaoCategoria.salvar('Frutas')
