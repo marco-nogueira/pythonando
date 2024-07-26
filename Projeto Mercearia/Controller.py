f3433a5e53e4b25e29fd47104866321008348d1d
Marco Nogueira <marco.nogueira@sistemafiep.org.br>
2024-07-25 21:13:46 -0300

    Adiciona ControllerCategoria, fim.

--- Projeto Mercearia/Controller.py
+++ Projeto Mercearia/Controller.py
@@ -33,7 +33,38 @@ class ControllerCategoria:
                 for i in x:
                     arq.writelines(i.categoria)
                     arq.writelines('\n')
-                    
+
+    def alterarCategoria(self, categoriaAlterar, categoriaAlterada):
+        x = DaoCategoria.ler()
+        
+        cat = list(filter(lambda x: x.categoria == categoriaAlterar, x))
+        
+        if len(cat) > 0:
+            cat1 = list(filter(lambda x: x.categoria == categoriaAlterada, x))
+            if len(cat1) == 0:
+                x = list(map(lambda x: Categoria(categoriaAlterada) if(x.categoria == categoriaAlterar) else(x),x))
+                print('A alteração foi efetuada com sucesso')
+            else:
+                print('A categoria que deseja alterar já existe')
+                
+        else:
+            print('A categoria que deseja alterar não existe')
+            
+        with open('Projeto Mercearia/categoria.txt', 'w') as arq:
+            for i in x:
+                arq.writelines(i.categoria)
+                arq.writelines('\n')
+            
+    def mostrarCategoria(self):
+        categorias = DaoCategoria.ler()
+        if(len(categorias)) == 0:
+            print('Não há categorias cadastradas')
+        else:
+            for i in categorias:
+                print(f'Categoria: {i.categoria}')
+                
 a = ControllerCategoria()
-# a.cadastrarCategoria('frios')
-a.removerCategoria('Frutas')
+# a.cadastrarCategoria('Frutas')
+# a.removerCategoria('Frutas')
+# a.alterarCategoria('Frutas', 'Carnes')
+a.mostrarCategoria()
